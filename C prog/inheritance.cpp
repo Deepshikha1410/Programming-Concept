854e700d128fbca453208110b1de7d9ff5bb6809
Deepshikha1410 <167551197+Deepshikha1410@users.noreply.github.com>
2024-07-15 18:47:49 +0530

    Add files via upload

---
+++ C prog/inheritance.cpp
@@ -0,0 +1,10 @@
+// Demonstrate single and multilevel inheritance using classes such as
+// Vehicle, Bike, Car etc.
+public class inheritance{
+    public static void main(string[] args){
+        public String Vehile
+        public String Bike
+        public String Car
+
+    }
+}
---
+++ C prog/prime.cpp
@@ -0,0 +1,2 @@
+//find the prime numbers between 1 and 10001 first using CPU.Mention the timing code in the program for checking prime number.
+    
